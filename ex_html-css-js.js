a4b6514c814dd28860087a7b1c48012c09b185ae
DaniHidalgo14 <dhidrod126@g.educaand.es>
2025-03-21 13:37:14 +0100

    rrrr

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,11 +1,10 @@
 //Escribe aquí tu código JavaScript
 let envio = document.getElementById("envio");
 envio.addEventListener("mouseover", function(){
-    if(envio.style.backgroundColor === "blue"){
-        envio.style.backgroundColor = "white";
-    }else{
-        envio.style.backgroundColor = "blue";
-    }
+    envio.style.backgroundColor = "white";
+})
+envio.addEventListener("mouseout", function(){
+    envio.style.backgroundColor = "blue"
 })
 
 document.getElementById("instrucciones").oninput = function(){
